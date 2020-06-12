338fa2d89d9018c28f3757b93d613813b408b38b
iceyKel <mcamo17@yahoo.com>
2020-06-12 20:08:30 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbase= "catchiecartdb";
+$servername = "den1.mysql2.gear.host";
+$username = "catchiecartdb3";
+$password = "Ku5M-A9z0j9_";
+$dbase= "catchiecartdb3";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbase);
